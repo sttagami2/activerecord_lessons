8dcb91353d0e43fa78dd4404c6f3e7e9c6e46537
vagrant <vagrant@localhost.localdomain>
2020-03-29 06:46:11 +0000

    [Finish] lesson4 レコードを挿入してみよう

--- main.rb
+++ main.rb
@@ -1,13 +1,27 @@
+require 'active_support/all'
 require 'active_record'
 require 'pp'
 
 Time.zone_default = Time.find_zone! 'Tokyo'
-ActiveRecord::base.default_timezone = :local
+ActiveRecord::Base.default_timezone = :local
 
 ActiveRecord::Base.establish_connection(
-  "adapter" => "sqlite3"
+  "adapter" => "sqlite3",
   "database" => "./myapp.db"
 )
 
 class User < ActiveRecord::Base
 end
+
+
+# insert
+
+user = User.new
+user.name = "tanaka"
+user.age = 23
+user.save
+
+user = User.new(name: "hayashi", age: 25)
+user.save
+
+User.create(name: "hoshi", age: 22)
