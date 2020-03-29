a18b6b4e471e76dbb531c26ec9b203ccf6f38e0d
vagrant <vagrant@localhost.localdomain>
2020-03-29 07:56:52 +0000

    [Finish] lesson7 抽出条件を設定

--- main.rb
+++ main.rb
@@ -22,7 +22,9 @@ User.create(name: "hayashi", age: 31)
 User.create(name: "mizutani", age: 28)
 User.create(name: "otsuka", age: 35)
 
-pp User.select("id, name, age").all
-pp User.select("id, name, age").first
-pp User.select("id, name, age").first(3)
-pp User.select("id, name, age").last
+# pp User.select("id, name, age").find(3)
+# pp User.select("id, name, age").find_by name: "tanaka"
+# pp User.select("id, name, age").find(3)
+# pp User.select("id, name, age").find_by_name("tanaka")
+# pp User.select("id, name, age").find_by_name("kiria")
+pp User.select("id, name, age").find_by_name!("kiria")
